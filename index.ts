a6319724ff5c819bf5d42ac3208c0ce37a778b7b
prajapatsachin7078 <sp639856@gmail.com>
2024-11-17 19:16:50 +0530

    learnt typescript basics

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
 let a:number= 5;
 console.log(a);
+// function parameter type declaration
+function printName(firstName:string|number|boolean){
+    console.log(firstName);
+}
+printName(23        );
+printName("hi")
+printName(true)
+
+// function return type declaration 
+function getSum(a:number,b:number):string{
+    return "" +a+b;
+}
+
+console.log(getSum(2,3));
